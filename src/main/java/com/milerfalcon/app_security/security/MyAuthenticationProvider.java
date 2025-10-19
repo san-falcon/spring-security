5931e7e91f48b65efb40ce85fb61b0e4822b5111
san-falcon <sanfalcon200@gmail.com>
2025-10-19 10:15:30 -0500

    Fin de la seccion 05

---
+++ src/main/java/com/milerfalcon/app_security/security/MyAuthenticationProvider.java
@@ -0,0 +1,45 @@
+package com.milerfalcon.app_security.security;
+
+import com.milerfalcon.app_security.repositories.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+@AllArgsConstructor
+public class MyAuthenticationProvider implements AuthenticationProvider {
+
+    private CustomerRepository customerRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        final var username = authentication.getName();
+        final var pwd = authentication.getCredentials().toString();
+
+        final var customerFromDb = this.customerRepository
+                .findByEmail(username)
+                .orElseThrow(() -> new BadCredentialsException("Invalid credentials"));
+
+
+        if (this.passwordEncoder.matches(pwd, customerFromDb.getPassword())) {
+            final var authorities = Collections
+                    .singletonList(new SimpleGrantedAuthority(customerFromDb.getRol()));
+            return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+        }
+        throw new BadCredentialsException("Invalid credentials");
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
--- src/main/java/com/milerfalcon/app_security/security/SecurityConfiguration.java
+++ src/main/java/com/milerfalcon/app_security/security/SecurityConfiguration.java
@@ -13,15 +13,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        // Por defecto utiliza esta configuracion
+
+        var requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
         httpSecurity
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/loans", "/about", "/balance", "/cards", "/account").authenticated()
@@ -29,6 +40,17 @@ public class SecurityConfiguration {
                 )
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
+
+        httpSecurity
+                .cors(cors -> corsConfigurationSource());
+
+        httpSecurity
+                .csrf(csrf -> csrf
+                        .csrfTokenRequestHandler(requestHandler)
+                        .ignoringRequestMatchers("/welcome", "/about")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
+
         return httpSecurity.build();
     }
 
@@ -50,18 +72,38 @@ public class SecurityConfiguration {
         return new JdbcUserDetailsManager(dataSource);
     }*/
 
-   /* @Bean
+    @Bean
     PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
-    }*/
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        var config = new CorsConfiguration();
+        // config.setAllowedOrigins(List.of("http://localhost:4200", "https://my-app-front-end.com"));
+        //? Con esto permitimos que todos los dominios puedan consumir nuestra API REST
+        config.setAllowedOrigins(List.of("*"));
+        // config.setAllowedMethods(List.of("POST", "GET", "PUT", "DELETE"));
+        config.setAllowedMethods(List.of("*"));
+
+        config.setAllowedHeaders(List.of("*"));
+
+        var source = new UrlBasedCorsConfigurationSource();
+        //? El primer parametro es para saber que ruta quiero proteger
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+
 
     /*
         ? Aca le estamos diciendo a Spring Security:
         ? “Usa este algoritmo (BCrypt) para comparar las contraseñas de usuarios.”
     */
-    @Bean
+    /*@Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
+    }*/
 
 }
